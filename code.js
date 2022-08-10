94a8e912ec4b955b0434f125dc81042de52852e3
= <=>
2022-08-10 19:56:47 -0300

    criando a base do projeto pokedex

--- code.js
+++ code.js
@@ -1,1 +1,43 @@
-ECHO est  ativado.
+const option = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default'
+}
+show = (info) => {
+    for (let n = 0; n < 151; n++) {
+        console.log(`${n} ${info.results[n].name}`)
+    }
+
+}
+
+let num = document.querySelector('.show')
+let valor = Number(num.innerHTML)
+const next = document.querySelector('.next')
+const back = document.querySelector('.back')
+next.addEventListener('click', (e) => {
+    valor += 1
+    num.innerHTML = valor
+    if (valor > 151) {
+        e.preventDefault()
+    }
+})
+back.addEventListener('click', (e) => {
+    valor -= 1
+    num.innerHTML = valor
+    if (valor < 0) {
+        e.preventDefault()
+    }
+})
+
+
+
+
+
+
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0', option)
+    .then(file => {
+        file.json()
+            .then(file => show(file))
+    })
+    .catch(() => { console.log('falhou') })
