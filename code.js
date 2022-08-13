c84b2727b3dd13df7b5b4f53d2b1e44a18fe255d
= <=>
2022-08-12 21:07:27 -0300

    adicionando toda a logica

--- code.js
+++ code.js
@@ -4,37 +4,27 @@ const option = {
     cache: 'default'
 }
 show = (info) => {
-    for (let n = 0; n < 151; n++) {
-        console.log(`${n} ${info.results[n].name}`)
+        for (let n = 0; n < info.count; n++) {
+            console.log(`${n} ${info.results[n].name}`)
+
+        }
     }
-
-}
-
-let num = document.querySelector('.show')
-let valor = Number(num.innerHTML)
-const next = document.querySelector('.next')
-const back = document.querySelector('.back')
-next.addEventListener('click', (e) => {
-    valor += 1
-    num.innerHTML = valor
-    if (valor > 151) {
-        e.preventDefault()
-    }
-})
-back.addEventListener('click', (e) => {
-    valor -= 1
-    num.innerHTML = valor
-    if (valor < 0) {
-        e.preventDefault()
-    }
-})
-
-
-
-
-
-
-
+    // let num = document.querySelector('.show')
+    // let valor = Number(num.innerHTML)
+    // const next = document.querySelector('.next')
+    // const back = document.querySelector('.back')
+    // next.addEventListener('click', () => {
+    //     if (valor < 151) {
+    //         valor += 1
+    //         num.innerHTML = valor
+    //     }
+    // })
+    // back.addEventListener('click', () => {
+    //     if (valor > 0) {
+    //         valor -= 1
+    //         num.innerHTML = valor
+    //     }
+    // })
 fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0', option)
     .then(file => {
         file.json()
